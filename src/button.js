23850ab243c8f6046bdd03c98daa40bd147264d3
tli-gini <ginitong.fish0524@gmail.com>
2023-11-16 23:24:09 +0800

    button

--- src/button.js
+++ src/button.js
@@ -1,4 +1,4 @@
-const guideBtns = document.querySelectorAll(".guide-button");
+const guideBtns = document.querySelectorAll(".guide-button", "#search-button");
 console.log(guideBtns);
 
 function guideBtnsOver(gBtn) {
@@ -6,4 +6,18 @@ function guideBtnsOver(gBtn) {
 }
 function guideBtnsOut(gBtn) {
   gBtn.style.backgroundColor = "#fff";
+}
+function searchBtnsOut(sBtn) {
+  sBtn.style.backgroundColor = "#f8f8f8";
+}
+
+const rightBtns = document.querySelectorAll(".right-icon");
+function rightBtnsOver(rBtn) {
+  rBtn.style.backgroundColor = "rgb(0 0 0 / 10%)";
+}
+function rightBtnsOut(rBtn) {
+  rBtn.style.backgroundColor = "#fff";
+}
+function voiceBtnsOut(vBtn) {
+  vBtn.style.backgroundColor = "rgb(0 0 0 / 5%)";
 }
